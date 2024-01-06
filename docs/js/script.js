bf7d650649aa28674e696655c1a4472f4f4e691c
bilal.aykis <bilal.aykis@gmail.com>
2024-01-06 18:32:24 +0300

    added data.json

--- docs/js/script.js
+++ docs/js/script.js
@@ -9,16 +9,22 @@ document.addEventListener('DOMContentLoaded', function () {
         filterSelect.innerHTML = '';
 
         if (selectedCategory !== 'Site Seçiniz') {
-            const filterOptions = [
-                selectedCategory + '_1',
-                selectedCategory + '_2'
-            ];
+            // data.json dosyasını fetch ile oku
+            fetch('data/data.json')
+                .then(response => response.json())
+                .then(jsonData => {
+                    const filteredPerson = jsonData.find(person => person.firstName === selectedCategory);
 
-            filterOptions.forEach(option => {
-                const newOption = document.createElement('option');
-                newOption.text = option;
-                filterSelect.add(newOption);
-            });
+                    if (filteredPerson) {
+
+                        (filteredPerson.hobbies.map(hobby => hobby)).forEach(option => {
+                            const newOption = document.createElement('option');
+                            newOption.text = option;
+                            filterSelect.add(newOption);
+                        });
+                    }
+                })
+                .catch(error => console.error('Data fetch error:', error));
         }
     }
 });
